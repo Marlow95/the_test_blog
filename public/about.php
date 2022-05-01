97e0b37aeacc80ef77b8e2d859000b182f086245
Marlow Collins <marlowcollins95@gmail.com>
2022-04-30 22:11:58 -0500

    pages updated

--- public/about.php
+++ public/about.php
@@ -1,4 +1,43 @@
 <?php require_once(__DIR__ . "/reusables/header.php") ?>
 <?php require_once(__DIR__ . "/reusables/navbar.php") ?>
-
+<div class="container">
+    <div class="row">
+        <div class="col m-4">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum condimentum dolor ac faucibus. Integer sem odio,
+            egestas vel libero non, iaculis consequat erat. Sed a neque ultricies, tincidunt elit sit amet, bibendum nibh. Aliquam dapibus 
+            consectetur ligula nec finibus. Aliquam ut ante leo. Mauris eget ex at ipsum dictum dictum. Nam convallis posuere libero, vitae 
+            feugiat sapien faucibus at. Morbi id est sed orci interdum tristique vitae et urna. Aenean metus justo, sagittis in ultricies id,
+            gravida id elit. Nunc et mollis ligula, vel tincidunt urna. Phasellus elementum tortor orci, iaculis aliquet tellus porta a. Donec
+            porttitor, libero et dignissim egestas, mi leo mattis odio, non pretium nisl ligula ac nisi. Vivamus non scelerisque diam. Suspendisse 
+            ultricies est eros, vel scelerisque sem fermentum egestas. Nam pellentesque, nisi id aliquam vestibulum, justo purus porta turpis, ac 
+            ullamcorper dolor orci in urna. Suspendisse fermentum luctus vestibulum.Cras rhoncus nibh sed nibh sollicitudin feugiat et ac libero. 
+            Suspendisse pulvinar leo vel magna porttitor, nec condimentum nunc posuere.Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nulla nibh dolor, sollicitudin non neque vitae, suscipit congue eros. Nunc quis maximus nulla. Fusce facilisis consectetur facilisis. 
+            Fusce a enim fringilla, faucibus elit ac, pharetra tellus. Praesent porta ex eget hendrerit aliquet. Etiam efficitur mauris sit amet 
+            mauris porttitor lacinia.
+            </p>
+            <p>
+            In eleifend diam purus, at faucibus nulla rutrum at. Vivamus lobortis turpis a velit dignissim, at ullamcorper sem iaculis. Duis ut lorem porttitor lacus molestie ultrices nec sit amet diam. Ut eget odio
+            lacus. Vivamus a orci lacus. Integer odio felis, auctor vel vulputate at, lacinia a leo. Etiam ex leo, porta at neque sit amet, vulputate dictum lorem.
+            Praesent sapien ipsum, luctus eget velit at, varius congue mi. Aliquam velit nunc, sodales ac luctus sit amet, molestie in augue. Curabitur vulputate 
+            egestas vulputate. Donec pharetra a felis eu consectetur. Fusce rutrum odio nec purus pretium, faucibus pulvinar diam pellentesque. Vestibulum non
+            tristique eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi sollicitudin,
+            enim et pretium euismod, turpis mauris consectetur nunc, in porta nulla velit non justo. Duis egestas id eros in egestas. Nunc
+            tincidunt et enim volutpat consequat. Etiam a aliquam lorem, ac vulputate purus. Morbi augue odio, elementum quis sapien id,
+            sodales viverra leo. Fusce non velit commodo, commodo tellus vel, congue justo. Cras condimentum viverra egestas. 
+            Nunc vehicula id neque eget consectetur.
+            </p>
+            <p>
+            Duis sagittis commodo nulla, at porttitor eros euismod in. Ut dui mauris, iaculis id erat mollis, porttitor maximus diam. Aenean fringilla 
+            nunc elit, a hendrerit justo volutpat eu. Sed rhoncus hendrerit nunc consectetur luctus. Donec pellentesque, quam eget aliquet faucibus, 
+            ipsum mauris maximus nulla, a blandit nunc erat eu arcu. Curabitur erat lectus, efficitur et felis vehicula, vulputate hendrerit magna. 
+            Pellentesque in fringilla orci, id tincidunt lectus. Aliquam auctor nisi sit amet turpis egestas placerat. Phasellus nec bibendum enim.
+            Praesent malesuada eleifend sem at placerat. Aliquam ac turpis et justo sollicitudin feugiat vel sit amet metus. Morbi dictum dui 
+            finibus leo auctor placerat. Proin eu egestas neque, id dignissim nisi. Morbi at tortor sollicitudin, bibendum felis et, ornare purus.
+            Donec massa mauris, varius id justo ac, tristique pretium nulla.
+            </p> 
+        </div>
+    </div>
+</div>
 <?php require_once(__DIR__ . "/reusables/footer.php") ?>
--- public/contact.php
+++ public/contact.php
@@ -1,4 +1,34 @@
 <?php require_once(__DIR__ . "/reusables/header.php") ?>
 <?php require_once(__DIR__ . "/reusables/navbar.php") ?>
 
+<div class="container">
+    <div class="row">
+        <h1>Contact Us</h1>
+        <div class="col m-4 border">
+            <form>
+            <div class="form-group m-4 row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group m-4 row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Fullname</label>
+                <div class="col-sm-10">
+                <input type="fullname" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group m-4 row">
+                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+            </div>
+            <div class="form-group m-4 row">
+                <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <?php require_once(__DIR__ . "/reusables/footer.php") ?>
