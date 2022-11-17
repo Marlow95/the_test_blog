73e1daf330808619ad941115dd0ec83345cd56d2
Marlow Collins <marlowcollins95@gmail.com>
2022-11-17 01:22:27 -0600

    production changes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,7 @@ session_start();
 
 //Database Connection
 require_once(__DIR__ . '/Database.php');
-//Imports ENV Variables
+//Imports Development ENV Variables
 require_once('../get_env.php');
 //Tmports PHPMail 
 require_once('../mail.php');
@@ -15,6 +15,9 @@ spl_autoload_register(fn($class)=> require_once('Model/'. $class . '.php'));
 
 
 //Database Created
+
+//Development database
+/*
 $db = new Database(
     $dotenv->load()['DB_TYPE'],
     $dotenv->load()['DB_HOST'],
@@ -22,9 +25,17 @@ $db = new Database(
     $dotenv->load()['DB_NAME'],
     $dotenv->load()['DB_USER'],
     $dotenv->load()['DB_PASS']
+); */
+
+//Production database
+$db = new Database(
+    $_ENV['DB_TYPE'],
+    $_ENV['DB_HOST'],
+    $_ENV['DB_PORT'],
+    $_ENV['DB_NAME'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS']
 );
-
-
 
 $db->connectDatabase();
 
